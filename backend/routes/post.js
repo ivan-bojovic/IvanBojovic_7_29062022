dbd99f6aa6c88d8beeacdc4c55afe43108b360bf
Ivan Bojovic <bojova281279@gmail.com>
2022-06-29 15:26:00 +0200

    ajout multer et express-bouncer

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,4 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config');
+
 const postCtrl = require('../controllers/post');
+
 
 
 
